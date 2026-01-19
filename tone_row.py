e3da18dcc6f3850e004d0257c2961fff9bc2d8dc
María A. Matienzo <maria@matienzo.org>
2026-01-18 18:22:12 -0800

    add more robust tone row generation

--- tone_row.py
+++ tone_row.py
@@ -1,14 +1,42 @@
 import copy
-from random import shuffle
+from typing import Literal, List
+from random import sample
 
-import abjad
+import abjad # pyright: ignore[reportMissingImports]
 
-# generate a random tone row
+BASE_ROW : List = list(range(12))
 
-BASE_ROW = list(range(12))
-randomized_row = copy.deepcopy(BASE_ROW)
-shuffle(randomized_row)
-tone_row = abjad.TwelveToneRow(items=randomized_row)
-reformatted = [tone.pitch_class_label().lower() for tone in tone_row.items]
-print(reformatted)
-# abjad.show(abjad.illustrate(tone_row))
+# convenenience method to return a randomized row
+def random_tone_row() -> abjad.TwelveToneRow: # pyright: ignore[reportArgumentType]
+    randomized_row = sample(BASE_ROW, len(BASE_ROW))
+    return abjad.TwelveToneRow(items=randomized_row)
+
+# reformat a string, Pitch, or PitchClass for the RTTTL
+# TODO: use abjad's respelling functionality because this is hacky
+def reformat_to_sharps(pitch: str | abjad.Pitch | abjad.PitchClass) -> str:
+    if isinstance(pitch, abjad.PitchClass):
+        pitch = pitch.pitch_class_label() # type: ignore[attr-defined]
+    elif isinstance(pitch, abjad.Pitch):
+        pitch = pitch.pitch_class().pitch_class_label()  # type: ignore[attr-defined]
+    lowercase = pitch.lower()
+
+    match lowercase:
+        case "ab":
+            return "g#"
+        case "bb":
+            return "a#"
+        case "eb":
+            return "d#"
+        case _:
+            return lowercase
+
+
+def main() -> None:
+    row = random_tone_row()
+    reformatted = [reformat_to_sharps(p) for p in row]
+    print(reformatted)
+    # abjad.show(abjad.illustrate(row))
+
+
+if __name__ == "__main__":
+    main()
