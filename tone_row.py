724421f194f6ec9763fe904d00accd7197cd89e1
María A. Matienzo <maria@matienzo.org>
2026-01-19 23:07:17 -0800

    expanded tonerow generation roughly a la the babbit timepoint set procedure

--- tone_row.py
+++ tone_row.py
@@ -33,10 +33,58 @@ def reformat_to_sharps(pitch: str | abjad.Pitch | abjad.PitchClass) -> str:
 
 def main() -> None:
     row = random_tone_row()
+    print(row)
     reformatted = [reformat_to_sharps(p) for p in row]
     print(reformatted)
-    # abjad.show(abjad.illustrate(row))
+    abjad.show(abjad.illustrate(row))
 
+def make():
+    row = random_tone_row()
+    # start with empty lists
+    notes, durations = [[], []]
+
+    # iterate over the row, getting index and PitchClass
+    for i, pc in enumerate(row):
+        d_this : int = pc.number()
+        d_next : int = row.items[(i + 1) % 12].number()
+        # this is a hack. confirm with the babbit paper
+        if d_next == 0:
+            d_next += 1 
+        # if we are on the first index, and we don't have a duration of 0, 
+        # insert a rest of that duration instesd
+        if (i == 0) and (d_this != 0):
+            notes.extend([[]])
+            durations.extend([abjad.Duration(d_this, 16)])
+        notes.extend([abjad.NamedPitch(pc)])
+        durations.extend([abjad.Duration(d_next, 16)])
+
+    # build out the staff structures from the note and duration lists
+    pitch_list = abjad.makers.make_pitch_lists(notes)
+    leaves = abjad.makers.make_leaves(
+        pitch_list, durations
+    )
+    # voice = abjad.Voice([staff], name="Piezo")
+
+    # check the duration, and figure out how many rests to add for it to be correct)
+    print(abjad.get.duration(leaves))
+    duration = abjad.get.duration(leaves).as_fraction()
+    final_rest, _ = abjad.duration.pair_with_denominator(duration, 16)
+    final_rest = 12 - final_rest % 12
+    print(final_rest)
+    
+    if final_rest != 0:
+        leaves += abjad.makers.make_leaves([[]], [abjad.Duration(final_rest, 16)])
+    
+
+    print(abjad.get.duration(leaves))
+    staff = abjad.Staff(leaves)
+    abjad.attach(abjad.TimeSignature((3, 4)), abjad.get.leaf(staff, 0), context=None)
+    staff.remove_commands().append("Note_head_engraver")
+    staff.consists_commands().append("Completion_head_engraver")
+    staff.remove_commands().append("Rest_engraver")
+    staff.consists_commands().append("Completion_rest_engraver")
+    score = abjad.Score([staff], name="flagday")
+    abjad.show(score)
 
 if __name__ == "__main__":
-    main()
+    make() 
