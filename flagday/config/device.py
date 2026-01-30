b2437f7816df86947e6164eaece496d800eb9d78
María A. Matienzo <maria@matienzo.org>
2026-01-30 11:53:37 -0800

    add all tests/refactor flagday.config.device

--- flagday/config/device.py
+++ flagday/config/device.py
@@ -11,12 +11,12 @@ import yaml
 
 from rtttl import parse_rtttl
 
-DEFAULT_BASE_CONFIG : str = os.path.join(os.getcwd(), "config", "base.yaml")
-DEVICE_CONFIG_MAX_LENGTH : dict[str, int] = {
+DEFAULT_BASE_CONFIG: str = os.path.join(os.getcwd(), "config", "base.yaml")
+DEVICE_CONFIG_MAX_LENGTH: dict[str, int] = {
     "owner": 40, "owner_short": 5, "ringtone": 230
 }
-INVALID_BASE_KEYS : Iterable[str] = DEVICE_CONFIG_MAX_LENGTH.keys()
-INVALID_BASE_SECURITY_KEYS : Iterable[str] = ["privateKey", "publicKey"]
+INVALID_BASE_KEYS: Iterable[str] = DEVICE_CONFIG_MAX_LENGTH.keys()
+INVALID_BASE_SECURITY_KEYS: Iterable[str] = ["privateKey", "publicKey"]
 
 
 class InvalidDeviceConfiguration(Exception):
@@ -36,22 +36,31 @@ def validate_base_config(cfg: dict[str, Any]) -> bool:
     """
     Gently validate a flagday Meshtastic device base config. In this case
     we are treating only the presence of INVALID_BASE_KEYS at the top level
-    and INVALID_BASE_SECURITY_KEYS under the `security` key as invalid 
-    
+    and INVALID_BASE_SECURITY_KEYS under the `security` key as invalid.
+
     :param cfg: a parsed device configuration
     :type cfg: dict[str, Any]
     :return: whether the configuration is valid
     :rtype: bool
     """
+    rv = None
     security_keys = cfg.get("security")
     if any(k in INVALID_BASE_KEYS for k in cfg):
-        return False
+        rv = False
+        raise InvalidDeviceConfiguration(
+            "owner, owner_short, or ringtone not valid for base configs"
+        )
     elif security_keys and any(
         k in INVALID_BASE_SECURITY_KEYS for k in security_keys
-        ):
-        return False
+    ):
+        rv = False
+        raise InvalidDeviceConfiguration(
+            "security.privateKey/security.publicKey not valid for base configs"
+        )
     else:
-        return True
+        rv = True
+    return rv
+
 
 def generate_device_config(
     base_config: dict[str, Any],
@@ -61,20 +70,23 @@ def generate_device_config(
 ) -> dict[str, Any]:
     """
     Generate a device configuration from a base config and other args.
-    
+
     :param base_config: a parsed base configuration
     :type base_config: dict[str, Any]
     :param owner: Meshtastic device owner name
     :type owner: str
-    :param owner_short: Meshtastic device owner shortname 
+    :param owner_short: Meshtastic device owner shortname
     :type owner_short: str
     :param ringtone: Unparsed RTTTL ringtone string
     :type ringtone: str
     :return: The merged device configuration
     :rtype: dict[str, Any]
-    :raises: InvalidDeviceConfiguration, rtttl.InvalidDefaultsError, rtttl.InvalidElementError, rtttl.InvalidNoteError, rtttl.InvalidRTTTLFormatError
+    :raises: InvalidDeviceConfiguration, rtttl.InvalidDefaultsError,
+             rtttl.InvalidElementError, rtttl.InvalidNoteError,
+             rtttl.InvalidRTTTLFormatError
     """
     device_config = copy.deepcopy(base_config)
+    validate_base_config(device_config)
     for prop, max_length in DEVICE_CONFIG_MAX_LENGTH.items():
         value = locals()[prop]
         if len(value.encode("utf-8")) > max_length:
@@ -83,7 +95,10 @@ def generate_device_config(
             )
         elif prop == "ringtone":
             # rtttl will raise exceptions if it's invalid
-            _ = parse_rtttl(ringtone, strict_note_syntax=True)
+            try:
+                _ = parse_rtttl(ringtone, strict_note_syntax=True)
+            except Exception:
+                raise
 
         device_config[prop] = value
 
--- tests/config/test_device.py
+++ tests/config/test_device.py
@@ -2,23 +2,73 @@
 Test device configuration methods.
 """
 
+import copy
 import unittest
+
+from pathlib import Path
 
 import yaml
 
-from flagday.config import device
+from flagday.config.device import (
+    generate_device_config, validate_base_config, InvalidDeviceConfiguration
+)
+
+
+FIXTURE_PATH: Path = Path(__file__).parent / 'example_base_good.yaml'
 
 
 class TestDeviceConfiguration(unittest.TestCase):
     def setUp(self) -> None:
-        with open('example_base_good.yaml', encoding="utf8") as file:
-            self.base_good = yaml.safe_load(file)
-        with open('example_base_bad_owner.yaml', encoding="utf8") as file:
-            self.base_bad_owner = yaml.safe_load(file)
-        with open('example_base_bad_owner_short.yaml', encoding="utf8") as file:
-            self.base_bad_owner_short = yaml.safe_load(file)
-        with open('example_base_bad_ringtone.yaml', encoding="utf8") as file:
-            self.base_bad_ringtone = yaml.safe_load(file)
-        self.ringtone_good = \
-            "beethoven:o=5,d=4,b=160:c,e,c,g,c,c6,8b,8a,8g,8a,8g,8f,8e,8f,8e,8d,c,e,g,e,c6,g."
+        self.owner_good = "girlscout420"
+        self.owner_short_good = "g420"
+        self.ringtone_good = "smbdeath:d=4,o=5,b=90:32c6,32c6,32c6,8p,16b,16f6,16p,16f6,16f6., 16e6.,16d6,16c6,16p,16e,16p,16c"  # noqa: E501
 
+        with open(FIXTURE_PATH, encoding="utf8") as file:
+            self.base_good = yaml.safe_load(file)
+            self.device_good = copy.deepcopy(self.base_good)
+            self.device_good["owner"] = self.owner_good
+            self.device_good["owner_short"] = self.owner_short_good
+            self.device_good["ringtone"] = self.ringtone_good
+
+    def test_validate_good_base_config(self) -> None:
+        self.assertTrue(validate_base_config(self.base_good))
+
+    def test_validate_bad_base_configs(self) -> None:
+        bad_base_configs = {
+            "owner": copy.deepcopy(self.base_good),
+            "owner_short": copy.deepcopy(self.base_good),
+            "ringtone": copy.deepcopy(self.base_good),
+            "security": copy.deepcopy(self.base_good)
+        }
+        bad_base_configs["owner"]["owner"] = self.owner_good
+        bad_base_configs["owner_short"]["owner_short"] = self.owner_short_good
+        bad_base_configs["ringtone"]["ringtone"] = self.ringtone_good
+        bad_base_configs["security"]["security"] = {
+            "privateKey": "foo", "publicKey": "bar"
+        }
+        for _, c in bad_base_configs.items():
+            with self.assertRaises(InvalidDeviceConfiguration):
+                validate_base_config(c)
+
+    def test_generate_device_config(self) -> None:
+        cfg = generate_device_config(
+            self.base_good,
+            owner=self.owner_good,
+            owner_short=self.owner_short_good,
+            ringtone=self.ringtone_good
+        )
+        self.assertEqual(cfg, self.device_good)
+        with self.assertRaises(InvalidDeviceConfiguration):
+            _ = generate_device_config(
+                self.base_good,
+                owner="blah"*230,
+                owner_short=self.owner_short_good,
+                ringtone=self.ringtone_good
+            )
+        with self.assertRaises(Exception):
+            generate_device_config(
+                self.base_good,
+                owner="blah"*230,
+                owner_short=self.owner_short_good,
+                ringtone="Invalid:d=4,o=5,b=90:F#6.,8F#.6,f#"
+            )
