7ee1da6df90ed364e8f48aa62a5a37a6063ec7a8
María A. Matienzo <maria@matienzo.org>
2026-01-30 11:54:28 -0800

    roll up flagday.composition.maker lint and push preamble to a lilypond include file

--- flagday/composition/maker.py
+++ flagday/composition/maker.py
@@ -1,4 +1,5 @@
 import copy
+import os
 import re
 from typing import List
 
@@ -11,59 +12,7 @@ from flagday.composition.series import (
     generate_random_series,
 )
 
-
-PREAMBLE: str = r"""#(set-global-staff-size 18)
-date = #(strftime "%Y-%m-%d" (localtime (current-time)))
-\header {
-    composer = \markup {
-        \override #'(font-name . "DINish Bold") "María Dolores A. Matienzo"
-    }
-    title = \markup { \override #'(font-name . "DINish Bold") "flagday" }
-    instrument = \markup {
-        \override #'(font-name . "DINish Regular")
-        \column {
-            \center-align {
-                \smaller {
-                    \override #'(font-name . "DINish SemiBold Italic")
-                    \line \italic { "for 4 or more piezoelectric buzzers" }
-                }
-                \vspace #0.5
-                \line {
-                    \override #'(font-name . "DINish SemiBold")
-                    \small  "(5'00\" ~ 20'00\")"}
-                \vspace #0.5
-                \line {
-                    \override #'(font-name . "DINish Regular Italic")
-                    \normal-text \smaller \italic
-                    "Piezoelectric buzzers should sound at irregular intervals during the piece's duration as if they were ringtones."
-                }
-                \vspace #2
-            }
-        }
-    }
-    tagline = ##f
-    copyright = \markup {
-        \override #'(font-name . "DINish Bold")
-        \smallCaps "© & ℗ 2026 Imprecision Art (ASCAP)"
-    }
-}
-\layout {
-    \context {
-        \Staff
-        \override VerticalAxisGroup.staff-staff-spacing.minimum-distance = #20
-    }
-    \context {
-        \Score
-        \override SystemStartBar.stencil = ##f
-    }
-}
-\paper {
-    page-count = 1
-    system-system-spacing.stretchability = #12
-    property-defaults.fonts.sans = "DINish Regular"
-    property-defaults.fonts.typewriter = "Cascadia Code PL"
-}
-"""
+PREAMBLE_FILE = os.path.join(os.getcwd(), 'stylesheets', 'preamble.ily')
 
 
 def make_series_notes(series: SeriesSeq) -> List[abjad.Note | abjad.Tuplet]:
@@ -194,7 +143,9 @@ def prepare_lilypond_file(
     # midi_block = abjad.Block("midi")
     layout_block = abjad.Block("layout")
     score_block = abjad.Block("score", [score, layout_block])
-    lilypond_file = abjad.LilyPondFile([PREAMBLE, score_block])
+    lilypond_file = abjad.LilyPondFile(
+        [rf'\include "{PREAMBLE_FILE}"', score_block]
+    )
     return lilypond_file
 
 
---
+++ stylesheets/preamble.ily
@@ -0,0 +1,51 @@
+#(set-global-staff-size 18)
+date = #(strftime "%Y-%m-%d" (localtime (current-time)))
+\header {
+    composer = \markup {
+        \override #'(font-name . "DINish Bold") "María Dolores A. Matienzo"
+    }
+    title = \markup { \override #'(font-name . "DINish Bold") "flagday" }
+    subtitle = \markup {
+        \override #'(font-name . "DINish Regular")
+        \column {
+            \center-align {
+                \smaller {
+                    \override #'(font-name . "DINish SemiBold Italic")
+                    \line \italic { "for 4 or more piezoelectric buzzers" }
+                }
+                \vspace #0.5
+                \line {
+                    \override #'(font-name . "DINish SemiBold")
+                    \small  "(5'00\" ~ 20'00\")"}
+                \vspace #0.5
+                \line {
+                    \override #'(font-name . "DINish Regular Italic")
+                    \normal-text \smaller \italic
+                    "Piezoelectric buzzers should sound at irregular intervals during the piece's duration as if they were ringtones."
+                }
+                \vspace #2
+            }
+        }
+    }
+    tagline = ##f
+    copyright = \markup {
+        \override #'(font-name . "DINish Bold")
+        \smallCaps "© & ℗ 2026 Imprecision Art (ASCAP)"
+    }
+}
+\layout {
+    \context {
+        \Staff
+        \override VerticalAxisGroup.staff-staff-spacing.minimum-distance = #20
+    }
+    \context {
+        \Score
+        \override SystemStartBar.stencil = ##f
+    }
+}
+\paper {
+    %page-count = 1
+    system-system-spacing.stretchability = #12
+    property-defaults.fonts.sans = "DINish Regular"
+    property-defaults.fonts.typewriter = "Cascadia Code PL"
+}
